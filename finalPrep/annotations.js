a91d482c32e7f0e8c93c5046f07c63ee611753c5
Katie Ammon <katie.ammon10@gmail.com>
2021-07-26 00:55:56 -0600

    Add exercises

--- finalPrep/annotations.js
+++ finalPrep/annotations.js
@@ -2,41 +2,61 @@
 // Use the double // to create a new comment
 
 //Build a Bear
+//here we have created a function, named it and assigned 5 perameters.
 function buildABear(name, age, fur, clothes, specialPower) {
+  //we have defined the first variable and assigned it at string using interpolation to insert the first perameter in the array.
   var greeting = `Hey partner! My name is ${name} - will you be my friend?!`;
+  // the second variable we have assigned to the first two perameters of the function.
   var demographics = [name, age];
+  //this variable is assigned to a string using concatenation to insert the "specialPower" perameter.
   var powerSaying = "Did you know that I can " + specialPower + " ?";
+  //this variable is assigned to an object with 4 key-value pairs.
   var builtBear = {
+    //this k-v-p pair calls the demographics variable which calls the first two perameters of the function.
     basicInfo: demographics,
+    //this k-v-p is assigned to the 3rd position of the function peerameter array
     clothes: clothes,
+    //this k-v-p is assigned to the 2nd position of the function peerameter array
     exterior: fur,
+    //this k-v-p is assigned to an int value.
     cost: 49.99,
+    //this key-value pair is assigned to an array that calls two variables and prints a string.
     sayings: [greeting, powerSaying, "Goodnight my friend!"],
+    //this key value pair is assigned to a boolean.
     isCuddly: true,
   };
-
+//this line will print the function "builtBear"
   return builtBear
 }
-
+//the syntax here appears to be wrong, however, this is attempting to assign values to the perameters
+//listed in the function.
 buildABear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares');
+
 buildABear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in');
 
 
 
 //FizzBuzz
+//here we have named a function and assigned it 3 perameters
 function fizzBuzz(num1, num2, range) {
+  //this is the for loop for the perameters
   for (var i = 0; i <= range; i++) {
+    //this line says that if num1 and num2 are even numbers, print "fizzbuzz"
     if (i % num1 === 0 && i % num2 === 0) {
       console.log('fizzbuzz');
+      //this says if just num1 is an even number, then print 'fizz'
     } else if (i % num1 === 0) {
       console.log('fizz');
+      //if just num2 is even(0 remainder), then pring buzz
     } else if (i % num2 === 0) {
       console.log('buzz');
+      //if both are odd, just  print "i".
     } else {
       console.log(i);
     }
   }
 }
-
+//this line is assigning values to the perameters of the function.
 fizzBuzz(3, 5, 100);
+//this line is assigning values to the perameters of the function.
 fizzbuzz(5, 8, 400);
--- finalPrep/modZeroHero.js
+++ finalPrep/modZeroHero.js
@@ -1,40 +1,66 @@
 // Challenge - See if you can follow the instructions and complete the exercise in under 30 minutes!
 
 // Declare two variables - heroName AND specialAbility - set to strings
-
+var heroName = "Tuck";
+var specialAbility = "Hiding"
 // Declare two variables - greeting AND catchphrase
+
 //   greeting should be assigned to a string that uses concatenation to include the heroName
 //   catchphrase should be assigned to a string that uses interpolation to include the specialAbility
-
+var greeting = ("hello" + " " + heroName);
+var chatchPhrase = `fast as a fox, I'm good at ${specialAbility}`;
 // Declare two variables - power AND energy - set to integers
 
 // Declare two variables - fullPower AND fullEnergy
 //   fullPower should multiply your current power by 500
 //   fullEnergy should add 150 to your current energy
-
+var fullPower = 5*500;
+var fullEnergy = 100+150;
 // Declare two variables - isHuman and identityConcealed - assigned to booleans
-
-
+var isHuman = true;
+var identityConcealed = false;
 // Declare two variables - archEnemies AND sidekicks
 //   archEnemies should be an array of at least 3 different enemy strings
 //   sidekicks should be an array of at least 3 different sidekick strings
+var archEnemies = ["duck", "squarls", "fish"];
+
+var sideKicks = ["dog", "lama", "lego"];
 
 // Print the first sidekick to your console
+console.log(sideKicks[0]);
 
 // Print the last archEnemy to the console
-
+console.log(archEnemies[2]);
 // Write some code to add a new archEnemy to the archEnemies array
-
+archEnemies.push("mouse");
 // Print the archEnemies array to console to ensure you added a new archEnemy
+console.log(archEnemies);
 
 // Remove the first sidekick from the sidekicks array
+sideKicks.shift();
 
 // Print the sidekicks array to console to ensure you added a new sidekick
-
+console.log(sideKicks);
 // Create a function called assessSituation that takes three arguments - dangerLevel, saveTheDay, badExcuse
 //   - dangerLevel should be an integer
-//   - saveTheDay should be a string a hero would say once they save the day 
+//   - saveTheDay should be a string a hero would say once they save the day
 //   - badExcuse should be a string a hero would say if they are too afraid of the dangerLevel
+function assessSituation(dangerLevel, saveTheDay, badExcuse){
+  dangerLevel = 80;
+  saveTheDay = "I'll always save you";
+  badExcuse = "Sorry!!, gotta live to fight another day!"
+  if (dangerLevel  > 50) {
+    console.log(badExcuse);
+  }
+  else if (dangerLevel >= 10) {
+    console.log(saveTheDay);
+  }
+
+  else  if (dangerLevel < 10) {
+    console.log("Meh. Hard pass");
+  }
+};
+console.log(assessSituation());
 
 // Your function should include an if/else statement that meets the following criteria
 //   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the badExcuse to the console
@@ -55,18 +81,55 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 //   - citiesDestroyed (array)
 //   - luckyNumbers (array)
 //   - address (object with following key/values: number , street , state, zip)
-
-
+var scaryMonster = {
+  name: "Lizard Man",
+  smell: "gut wrenching",
+  weight: "500 lbs",
+  citiesDestroyed: ["San Antonio", "Dallas", "Indianapolis"],
+  luckyNumbers: [2, 42, 72],
+  address: {
+    number:  36,
+    street:  "River Run dr",
+    state:  "UT",
+    zip:  47201,
+  }
+}
+console.log(scaryMonster);
 // Create a new class called SuperHero
+
+
 // - Your class should have the following DYNAMIC values
-//   - name 
+//   - name
 //   - superpower
-//   - age 
+//   - age
 // - Your class should have the following STATIC values
 //   - archNemesis, assigned to "The Syntax Error"
 //   - powerLevel = 100
-//   - energyLevel = 50 
+//   - energyLevel = 50
+class SuperHero {
+  archNemesis = "The Syntax Error";
+  powerLevel = 100;
+  energyLevel = 50;
+  constructor(name, superpower, age,) {
+    this.name = name;
+    this.superpower = superpower;
+    this.age = age;
+  }
+  sayName() {
+    console.log("Sandy");
+  }
+  maximizeEnergy(){
+    energyLevel = 1000;
+  }
+  gainPower(){
 
+  }
+}
+var non = new SuperHero("Luis", "Kicking", 28);
+var con = new SuperHero("Sandy", "Flying", 32);
+con.gainPower();
+console.log(con);
+console.log(non);
 // - Create the following class methods
 //   - sayName, should print the hero's name to the console
 //   - maximizeEnergy, should update the energyLevel to 1000
@@ -77,8 +140,12 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 
 // Reflection
 // What parts were most difficult about this exerise?
+// I wasn't able to create the gainPower method. I'm struggling to understand how methods interact with values in an object or class.
+
 
 // What parts felt most comfortable to you?
+//I feel really good about making and calling variables, objects and classes.
 
 // What skills do you need to continue to practice before starting Mod 1?
-
+// Interacting methods and functions with values and variables. Memorizing the syntax for concatenation and interpolation as well as
+//syntax for loops and functions.
